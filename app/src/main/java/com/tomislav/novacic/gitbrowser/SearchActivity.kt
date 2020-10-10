37989e9c82624ce96777d5a20d5b3da9df7f13f4
TomislavNovačić <tomislav.novacic2@gmail.com>
2020-10-10 20:21:55 +0200

    Renamed MainActivity to SearchActivity

--- app/src/main/java/com/tomislav/novacic/gitbrowser/SearchActivity.kt
+++ app/src/main/java/com/tomislav/novacic/gitbrowser/SearchActivity.kt
@@ -1,11 +1,11 @@
 package com.tomislav.novacic.gitbrowser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
     }
 }
